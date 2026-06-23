9b857ce3e355fcec0239eb2d4efc0b6b6dee419f
mrshahsuvie <mrshah@suvie.com>
2026-06-23 19:50:08 +0530

    Register Spreedly gateway as a payment method type
    -Append SolidusSpreedly::Gateway to config.spree.payment_methods so it
    appears in the admin New Payment Method type dropdown, and permit the
    payment_method_token / payment_method_type source attributes submitted by
    the storefront.
    -Permit SolidusSpreedly::Response for payment log-entry deserialization
    and add the ActiveRecord model translation so the admin displays
    Spreedly instead of Solidus spreedly/gateway.
    -Update the README with installation and configuration instructions

--- lib/solidus_spreedly/engine.rb
+++ lib/solidus_spreedly/engine.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'solidus_core'
-require 'solidus_support'
+require "solidus_core"
+require "solidus_support"
 
 module SolidusSpreedly
   class Engine < Rails::Engine
@@ -9,7 +9,30 @@ module SolidusSpreedly
 
     isolate_namespace ::Spree
 
-    engine_name 'solidus_spreedly'
+    engine_name "solidus_spreedly"
+
+    # Register the gateway as an available payment method type (so it shows up
+    # in the admin "New Payment Method" dropdown) and permit the Spreedly source
+    # attributes the storefront submits. `config.spree.payment_methods` is a
+    # ClassConstantizer::Set, so it dedups the entry by class name.
+    initializer "solidus_spreedly.register_payment_method", after: "spree.register.payment_methods" do |app|
+      config.to_prepare do
+        app.config.spree.payment_methods << "SolidusSpreedly::Gateway"
+
+        ::Spree::PermittedAttributes.source_attributes.concat(
+          %i[payment_method_token payment_method_type]
+        ).uniq!
+      end
+    end
+
+    # Allow the Spreedly response wrapper to be deserialized from payment log
+    # entries (ActiveMerchant::Billing::Response is already permitted by core).
+    initializer "solidus_spreedly.log_entry_permitted_classes" do
+      Spree.config do |config|
+        config.log_entry_permitted_classes << "SolidusSpreedly::Response"
+        config.log_entry_permitted_classes.uniq!
+      end
+    end
 
     # use rspec for tests
     config.generators do |g|
